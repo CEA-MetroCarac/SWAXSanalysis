38bcf4c3abb383b2468f8f93a0d2a689ba52c977
AdrienT34490 <toulouseadrienjob@gmail.com>
2025-01-02 15:34:26 +0100

    New version

---
+++ edf2nxsas/nexus_formats/__init__.py
@@ -0,0 +1,5 @@
+"""
+This folder lets you :
+    - Generate a NeXus format via the file nexus_format_generator.py
+    - stock the various NeXus format created and used in the conversion from edf to the specified NeXus format
+"""
