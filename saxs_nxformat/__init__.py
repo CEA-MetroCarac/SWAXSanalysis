9c13763a92b16440c373cf06180a7f4b07eef2f5
AdrienT34490 <toulouseadrienjob@gmail.com>
2025-03-06 15:24:20 +0100

    Change CMAP
    I chose a cmap that doesn't go to pure white or a very light yellow. That way it's still visible in plot with multiple lines

--- saxs_nxformat/__init__.py
+++ saxs_nxformat/__init__.py
@@ -1,5 +1,5 @@
 """
-TODO : Add global variable for colormap, .ico path and other parameters
+TODO : Add global variable for colormap, .ico path and other parameters, font, fontsize...
 """
 import os
 import sys
@@ -32,7 +32,7 @@ IPYNB_PATH = DTC_PATH / "Notebooks"
 ICON_PATH = BASE_DIR / "Images" / "nxformat_icon.ico"
 
 # Global variables to the file
-PLT_CMAP = "magma"
+PLT_CMAP = "plasma"
 PLT_CMAP_OBJ = plt.get_cmap(PLT_CMAP)
 json_path = BASE_DIR / "nexus_standards" / "structure_NXunits.json"
 with open(json_path, "r", encoding="utf-8") as file_dict:
