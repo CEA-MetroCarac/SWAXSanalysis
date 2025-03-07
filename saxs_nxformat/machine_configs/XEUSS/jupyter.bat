bcab89f56cc4535113ecca81f7cf2b356ed088c7
AdrienT34490 <toulouseadrienjob@gmail.com>
2025-03-07 15:20:11 +0100

    Update jupyter.bat

--- saxs_nxformat/machine_configs/XEUSS/jupyter.bat
+++ saxs_nxformat/machine_configs/XEUSS/jupyter.bat
@@ -1,4 +1,5 @@
 cd C:/Python310/envs/env_nxformat/Scripts
 
-:: change AB123456 to your user tag
-jupyter notebook --notebook-dir=C:\Users\AB123456\Desktop
+:: Change this path to where you would like to open jupyter, don't forget to put \\ instead of \
+set working_dir=C:\\Users\\AB123456\\Desktop
+jupyter notebook --notebook-dir=%working_dir%
