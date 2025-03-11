0f68279bd4dbec4dde39bd99eb5f5a309878190a
AdrienT34490 <150667766+AdrienT34490@users.noreply.github.com>
2025-03-11 11:46:48 +0100

    Delete saxs_nxformat/machine_configs/XEUSS/jupyter.bat

--- saxs_nxformat/machine_configs/XEUSS/jupyter.bat
+++
@@ -1,5 +0,0 @@
-cd C:/Python310/envs/env_nxformat/Scripts
-
-:: Change this path to where you would like to open jupyter, don't forget to put \\ instead of \
-set working_dir=C:\\Users\\AB123456\\Desktop
-jupyter notebook --notebook-dir=%working_dir%
